00c70fa3625ee6edb49113a1157aa2a0231f6e39
mengfanjin0317 <you@example.com>
2018-04-25 14:53:28 +0900

    management修改

---
+++ resources/views/management.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<!-- <head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/4.0.0-beta/css/bootstrap.min.css">
+  <script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdn.bootcss.com/popper.js/1.12.5/umd/popper.min.js"></script>
+  <script src="https://cdn.bootcss.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
+</head> -->
+<body>
+
+<div class="container">
+  <h2>マネジメント登録</h2>
+  <form>
+    <div class="form-group">
+      <label for="name">名前:</label>
+      <input type="text" class="form-control" id="name" placeholder="Enter name">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+    </div>
+    <div class="form-check">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</div>
+
+</body>
+</html>
